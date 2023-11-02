1725c39b2d185d4ce080ea483af72cf053fea97a
urlordjames <32751441+urlordjames@users.noreply.github.com>
2023-11-02 15:37:27 -0400

    add script to calculate guassian distribution

---
+++ gauss.rs
@@ -0,0 +1,12 @@
+fn main() {
+	let gausses: Vec<f32> = (-8..=8).map(gauss).collect();
+	println!("{gausses:?}");
+}
+
+const SIGMA: f32 = 17.0 / (2.0 * std::f32::consts::PI);
+const VARIANCE: f32 = SIGMA * SIGMA;
+
+fn gauss(n: i8) -> f32 {
+	let n = n as f32;
+	(1.0 / (std::f32::consts::TAU * VARIANCE).sqrt()) * (-(n * n) / (2.0 * VARIANCE)).exp()
+}

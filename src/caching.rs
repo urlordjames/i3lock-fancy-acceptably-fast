d94150a94258334ae3d4d37a061f7dc8cf60b260
urlordjames <32751441+urlordjames@users.noreply.github.com>
2023-11-06 10:57:49 -0500

    version kernel

--- src/caching.rs
+++ src/caching.rs
@@ -1,17 +1,23 @@
 const KERNEL_SRC: &str = include_str!("kernel.cl");
+const KERNEL_VERSION: &str = "1";
 
-// TODO: version kernel
 pub fn get_program_cached(device: ocl::Device, ctx: &ocl::Context) -> ocl::Program {
 	let home = std::path::PathBuf::from(std::env::var("HOME").unwrap());
 	let cache_dir = home.join(".cache").join("i3lock-faf");
 	std::fs::create_dir_all(&cache_dir).unwrap();
 
+	let cache_version = cache_dir.join("version.txt");
+	let version = std::fs::read_to_string(&cache_version).unwrap_or(String::from("0"));
+
 	let cache_file = cache_dir.join("kernel.bin");
-	std::fs::read(&cache_file).ok().and_then(|binary| ocl::Program::builder()
-		.binaries(&[&binary])
-		.devices(device)
-		.build(ctx).ok()
-	).unwrap_or_else(|| {
+	std::fs::read(&cache_file).ok().and_then(|binary| {
+		if KERNEL_VERSION != version { return None; }
+
+		ocl::Program::builder()
+			.binaries(&[&binary])
+			.devices(device)
+			.build(ctx).ok()
+	}).unwrap_or_else(|| {
 		let program = ocl::Program::builder()
 			.src(KERNEL_SRC)
 			.devices(device)
@@ -22,6 +28,7 @@ pub fn get_program_cached(device: ocl::Device, ctx: &ocl::Context) -> ocl::Progr
 				let bin = bins.pop().unwrap();
 				if bins.pop().is_some() { panic!("a single device should only produce one binary"); }
 				std::fs::write(cache_file, bin).unwrap();
+				std::fs::write(cache_version, KERNEL_VERSION).unwrap();
 			},
 			_ => unreachable!("not very rusty, but I'll work with it")
 		}
